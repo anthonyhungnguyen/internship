b2a53c428915220750cbda75fb903a43d13bfb4a
PhucHung <anthonyhungnguyen276@gmail.com>
2020-08-05 20:24:10 +0700

    fixing bugs

--- module-2-linux-database-deployments/source/insert.py
+++ module-2-linux-database-deployments/source/insert.py
@@ -6,15 +6,22 @@ from utilities import *
 
 Base = declarative_base()
 
-engine = create_engine('mysql+pymysql://ngph:Hung8787@localhost/m2', echo=True)
+# WORK
+# engine = create_engine('mysql+pymysql://ngph:Hung8787@localhost/m2', echo=True)
+
+# HOME
+engine = create_engine('mysql+pymysql://ngph:ngph@localhost/m2', echo=True)
+
 
 class User(Base):
     __tablename__ = 'user'
 
-    userId = Column('userID', String(32), primary_key=True)
-    userRecency = Column('userRecency', Date)
-    userFrequency = Column('userFrequency', Integer, default=0)
-    userMonetary = Column('userMonetary', Float, default=0)
+    id = Column('id', String(32), primary_key=True)
+    recorded_date = Column('recorded_date', Date,
+                           primary_key=True, default=datetime.datetime.now().date)
+    recency = Column('recency', Integer, default=None)
+    frequency = Column('frequency', Integer, default=0)
+    monetary = Column('monetary', Float, default=0)
     transactions = relationship('Transaction')
     userpayapp = relationship('UserPayApp')
 
@@ -22,50 +29,80 @@ class User(Base):
 class Bank(Base):
     __tablename__ = 'bank'
 
-    bankCode = Column('bankCode', String(10), primary_key=True)
-    recordedDate = Column('date', Date, primary_key=True, )
-    successTrans = Column('successTrans', Integer, default=0)
-    totalTrans = Column('totalTrans', Integer, default=0)
+    bank_code = Column('bank_code', String(10), primary_key=True)
+    recorded_date = Column('recorded_date', Date,
+                           primary_key=True, default=None)
+    success_trans = Column('success_trans', Integer, default=0)
+    total_trans = Column('total_trans', Integer, default=0)
     transactions = relationship('Transaction')
+
 
 class Transaction(Base):
     __tablename__ = 'transaction'
 
-    transID = Column('transID', String(32), primary_key=True)
-    userID = Column('userID', String(32), ForeignKey('user.userID'))
-    reqDate = Column('reqDate', Date, nullable=False)
-    bankCode = Column('bankCode', ForeignKey('bank.bankCode'))
+    id = Column('id', String(32), primary_key=True)
+    user_id = Column('user_id', String(32), ForeignKey('user.id'))
+    req_date = Column('req_date', Date, nullable=False)
+    bank_code = Column('bank_code', ForeignKey('bank.bank_code'))
     amount = Column('amount', Float, nullable=False)
-    transStatus = Column('transStatus', Integer, nullable=False)
+    trans_status = Column('trans_status', Integer, nullable=False)
     type = Column('type', Integer, nullable=False)
-
 
 
 class UserPayApp(Base):
     __tablename__ = 'user_pay_app'
 
-    userID = Column('userID', String(32), ForeignKey('user.userID'), primary_key=True)
-    appID = Column('appID', Integer, primary_key=True)
-    totalAmount = Column('totalAmount', Float)
+    user_id = Column('user_id', String(32), ForeignKey(
+        'user.id'), primary_key=True)
+    app_id = Column('app_id', Integer, primary_key=True)
+    total_amount = Column('total_amount', Float)
+
 
 def insert_users(session):
-    all_users = return_users()
-    for u in all_users:
+    allUsers = return_users()
+    for u in allUsers:
         new_user = User()
-        new_user.userId = u
+        new_user.id = u[0]
+        new_user.recency = u[1]
+        new_user.frequency = u[2]
+        new_user.monetary = u[3]
         session.add(new_user)
-    session.commit()
+
 
 def insert_bank(session):
-    all_banks = return_banks()
-    for b in all_banks:
-        new_bank = Bank()
-        new_bank.bankCode = b[0]
-        new_bank.recordedDate = b[1]
-        new_bank.successTrans = b[2]
-        new_bank.totalTrans = b[3]
-        session.add(new_bank)
-    session.commit()
+    allBanks = return_banks()
+    for b in allBanks:
+        newBank = Bank()
+        newBank.bank_code = b[0]
+        newBank.recorded_date = b[1]
+        newBank.success_trans = b[2]
+        newBank.total_trans = b[3]
+        session.add(newBank)
+
+
+def insert_userpayapp(session):
+    userPayApp = return_rfm()
+    for upa in userPayApp:
+        new_upa = UserPayApp()
+        new_upa.user_id = upa[0]
+        new_upa.app_id = upa[1]
+        new_upa.total_amount = upa[2]
+        session.add(new_upa)
+
+
+def insert_transactions(session):
+    transactions = return_transactions()
+    for t in transactions:
+        new_t = Transaction()
+        new_t.user_id = t[0]
+        new_t.req_date = t[1]
+        new_t.id = t[2]
+        new_t.bank_code = t[3]
+        new_t.amount = t[4]
+        new_t.trans_status = t[5]
+        new_t.type = t[6]
+        session.add(new_t)
+
 
 # Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
@@ -73,10 +110,9 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # insert_users(session)
-insert_bank(session)
+# insert_bank(session)
+# insert_userpayapp(session)
+insert_transactions(session)
 
-# users = session.query(User).all()
-# for user in users:
-#     print(user.id)
-# session.commit()
+session.commit()
 session.close()
--- module-2-linux-database-deployments/source/utilities.py
+++ module-2-linux-database-deployments/source/utilities.py
@@ -4,18 +4,45 @@ from datetime import timedelta
 import numpy as np
 
 df = pd.read_csv('./vtf2020_module2_data.csv')
-df['reqDate'] = df['reqDate'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f').date())
+df['reqDate'] = df['reqDate'].apply(
+    lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S.%f').date())
+
 
 def return_users():
-    return df['userID'].unique()
+    max_date = max(df['reqDate'])
+    rfm = df.groupby(['userID']).agg({
+        'reqDate': lambda x: (max_date + timedelta(1) - x.max()).days,
+        'transID': 'count',
+        'amount': 'sum'
+    })
+    rfm.columns = ['recency', 'frequency', 'monetary']
+    rfm['r-score'] = pd.qcut(rfm['recency'], q=4, labels=range(4, 0, -1))
+    rfm['f-score'] = pd.qcut(rfm['frequency'], q=4, labels=range(1, 5, 1))
+    rfm['m-score'] = pd.qcut(rfm['monetary'], q=4, labels=range(1, 5, 1))
+    rfm.reset_index(inplace=True)
+    return rfm.values
+
 
 def return_banks():
-    sorted_date = sorted(df['reqDate'].unique())
     bank_success = df.groupby(['bankCode', 'reqDate']).agg({
         'transStatus': 'sum',
         'transID': 'count'
-    })  
+    })
 
     bank_success.columns = ['successTransactions', 'totalTransactions']
-    bank_success['SuccessRate'] = bank_success['successTransactions'] / bank_success['totalTransactions']
+    bank_success['SuccessRate'] = bank_success['successTransactions'] / \
+        bank_success['totalTransactions']
     return bank_success.reset_index().values
+
+
+def return_rfm():
+    appid = df.loc[~df['type'].isin([1, 2, 3, 4])]
+    appid = appid.groupby(['userID', 'type']).agg({
+        'amount': 'sum'
+    })
+    appid.reset_index(inplace=True)
+    return appid.values
+
+
+def return_transactions():
+    return df.values
