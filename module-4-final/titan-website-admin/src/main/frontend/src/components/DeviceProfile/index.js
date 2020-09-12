3e6c83ced1e7dd855ee78874f0b18bcfff8c323a
AnthonyHungNguyen <anthonyhungnguyen276@gmail.com>
2020-09-12 15:12:00 +0700

    add loading screen when server error

--- module-4-final/titan-website-admin/src/main/frontend/src/components/DeviceProfile/index.js
+++ module-4-final/titan-website-admin/src/main/frontend/src/components/DeviceProfile/index.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Search from './Search'
-import { Row, Col } from 'antd'
-import { useDispatch } from 'react-redux'
+import { Row, Col, Skeleton } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchActivity } from '../../slices/deviceActivity'
 import { fetchConnection } from '../../slices/deviceConnection'
-import { fetchDevice } from '../../slices/device'
+import { deviceSelector, fetchDevice } from '../../slices/device'
 import './index.css'
 export default () => {
 	const dispatch = useDispatch()
 	const [ currentDeviceId, setCurrentDeviceId ] = useState('EDF10704-E7E4-4CC6-BA25-9A30C7720D02')
+	const { hasErrors } = useSelector(deviceSelector)
 
 	useEffect(
 		() => {
@@ -19,11 +20,13 @@ export default () => {
 		[ dispatch, currentDeviceId ]
 	)
 
-	return (
+	return !hasErrors ? (
 		<Row gutter={[ 24, 24 ]}>
 			<Col span={24}>
 				<Search currentDeviceId={currentDeviceId} setCurrentDeviceId={setCurrentDeviceId} />
 			</Col>
 		</Row>
+	) : (
+		<Skeleton active />
 	)
 }
