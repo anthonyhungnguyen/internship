caaf7c9bf6a44fb1136cfdbd9314a7629acc0a59
ngph <anthonyhungnguyen276@gmail.com>
2020-09-21 17:31:30 +0700

    process data + add AppIDFreq

---
+++ module-4-final/titan-website-admin/src/main/frontend/src/components/DeviceProfile/Activity/MerchantFrequency/GeneralView/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Card, Modal } from 'antd'
+import { deviceActivitySelector } from '../../../../../slices/deviceActivity'
+import ReactEchartsCore from 'echarts-for-react/lib/core'
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/pie'
+import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons'
+
+
+export default React.memo(() => {
+	const { merchantFrequency } = useSelector(deviceActivitySelector)
+	const [visible, setVisible] = useState(false)
+
+	const getOption = () => {
+		return {
+			tooltip: {
+				trigger: 'item',
+				formatter: '{b} : {c} ({d}%)'
+			},
+			series: [
+				{
+					type: 'pie',
+					selectedMode: 'multiple',
+					data: merchantFrequency.map((mf) => ({
+						name: `${mf.merchant} - ${mf.merchant_count}`,
+						value: mf.merchant_count
+					})),
+					animation: true,
+					label: {
+						position: 'outer',
+						alignTo: 'none',
+						bleedMargin: 5
+					}
+				}
+			],
+			emphasis: {
+				itemStyle: {
+					shadowBlur: 10,
+					shadowOffsetX: 0,
+					shadowColor: 'rgba(0, 0, 0, 0.5)'
+				}
+			}
+		}
+	}
+
+	const handleToggleVisible = () => {
+		setVisible((old) => !old)
+	}
+
+	return (
+		<React.Fragment>
+			<Card
+				title="Merchant Frequency"
+				headStyle={{ fontWeight: 'bold', fontSize: '1.3em' }}
+				hoverable={true}
+				extra={
+					<button onClick={handleToggleVisible}>
+						{visible ? (
+							<FullscreenExitOutlined className="text-xl" />
+						) : (
+								<FullscreenOutlined className="text-xl" />
+							)}
+					</button>
+				}
+			>
+				<ReactEchartsCore echarts={echarts} option={getOption()} />
+			</Card>
+			<Modal
+				title="Merchant Frequency"
+				visible={visible}
+				onOk={handleToggleVisible}
+				onCancel={handleToggleVisible}
+				centered
+				width={1000}
+				footer={null}
+			>
+				<ReactEchartsCore echarts={echarts} option={getOption()} style={{ height: '70vh', width: '100%' }} />
+			</Modal>
+		</React.Fragment>
+	)
+})
--- module-4-final/vng/src/main/java/com/example/vng/repository/ArangoDBCustom.java
+++ module-4-final/vng/src/main/java/com/example/vng/repository/ArangoDBCustom.java
@@ -14,8 +14,7 @@ public class ArangoDBCustom {
 
     public ArrayList<Object> getDataByDynamicQuery(CustomQuery customQuery) {
         Map<String, Object> bindVars = customQuery.getBindVars();
-        Map<String, Object> options = customQuery.getOptions();
-        String query = String.format(customQuery.getQuery(), options.get("direction"), options.get("collect"), options.get("return"));
+        String query = customQuery.getQuery();
         System.out.println(query);
         return new ArrayList<>(arangoDB.db("_system").query(query, bindVars, null, Object.class).asListRemaining());
     }
